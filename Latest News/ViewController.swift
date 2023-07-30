dd3a3e6707ecdd311da5488605de20fb332fcc51
Soha Ahmed Hamdy <soha7hmed@gmail.com>
2023-07-30 21:34:15 +0300

    Initial Commit

---
+++ Latest News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Latest News
+//
+//  Created by Soha Ahmed Hamdy on 30/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
